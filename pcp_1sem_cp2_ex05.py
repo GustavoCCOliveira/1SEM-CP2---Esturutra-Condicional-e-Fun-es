9a00f17b7605b3b32447b034d56a947cf1fd8cf8
GustavoCCOliveira <gustavotorresdeoliveira20@gmail.com>
2026-04-23 09:00:08 -0300

    Terminado o codigo do exercicio 5 da cp, de forma que ele execute a calculadora de parcelas corretamente.

--- pcp_1sem_cp2_ex05.py
+++ pcp_1sem_cp2_ex05.py
@@ -1,17 +1,41 @@
 #Criar um programa aonde ele consiga analizar Nome, idade, valor de renda mensal, valor desejado do emprestimo, numero de parcelas (3 ate 24 )
-nome = input("Digite seu nome: ")
-idade = int(input("Digite sua idade: "))
-renda = int(input("Digite sua renda mensal: "))
+print("=== Cadastro para Empréstimo ===")
+print("Preencha as informações abaixo:\n")
+
+nome = input("Nome: ")
+idade = int(input("Idade: "))
+renda = float(input("Renda mensal: "))
 emprestimo = float(input("Valor do empréstimo: "))
-parcelas = int(input("Número de parcelas (3 a 24): "))
+parcelas = int(input("Parcelas (3 a 24): "))
+
 #Agora o maximo de emprestimo e a renda x 20
 maximo = renda * 20
+if idade <= 18:
+    print("Empréstimo negado: menor de idade")
+    exit()
+
+if emprestimo > maximo:
+    print("Empréstimo negado: valor excede 20x sua renda")
+    exit()
+
 if parcelas <= 6:
-    print("Seu valor e de 5% de parcelas por mes")
-#tenho que resolver a formula das parcelas e a resolucao pra a quantidade de parcelas extras
-#agora adicionar o valor do financiamento
-if idade < 18:
-     print("Idade invalida!")
+    i = 0.05
+elif parcelas <= 12:
+    i = 0.08
+elif parcelas <= 24:
+    i = 0.10
 else:
-    if idade >= 18:
-     print("Seu emprestimo gera um valor de ")
+    print("Número de parcelas inválido")
+    exit()
+
+#PMT formula da renda = PV . i(1+i)n/ (1+i)n-1
+n = parcelas
+PV = emprestimo
+
+PMT = PV * (i * (1 + i) ** n) / ((1 + i) ** n - 1)
+print("|============================|")
+print(f" Cliente: {nome}")
+print(f" Empréstimo aprovado!\n")
+print(f" Valor da parcela: R$ {PMT:.2f}\n")
+print(f" Total pago: R$ {PMT * n:.2f}\n")
+print("|============================|")
